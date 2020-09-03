e3c931ed060cd5f3bb8c68e286eaa64174bffdc7
Nitin Sijwali <sijwalinitin@gmail.com>
2020-09-04 00:04:02 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+checkout---> https://clone-8a0d0.web.app/
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,21 +1,21 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import API_KEY from './Keys';
 
 const CONTEXT_KEY = "YOUR_CONTEXT_KEY";
 
 const useGoogleSearch = (term) => {
     const [data, setData] = useState(null);
-    useEffect(()=>{
-        const fetchData =async() => {
+    useEffect(() => {
+        const fetchData = async () => {
             fetch(
                 `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
             )
-            .then(response => response.json())
-            .then(result => {
-                setData(result)
-            })
-            .catch((error) => {
-                console.log('erroor>>>>',error)
+                .then(response => response.json())
+                .then(result => {
+                    setData(result)
+                })
+                .catch((error) => {
+                    console.log('erroor>>>>', error)
                 });
         }
 
